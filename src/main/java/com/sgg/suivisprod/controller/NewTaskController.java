c9a281ac268d6832f0c082709d17a2bfb62ec5ec
Eric RANDRIANARISOA <raneric@gmail.com>
2022-04-03 14:42:25 +0300

    fix error handler on login page

--- src/main/java/com/sgg/suivisprod/controller/NewTaskController.java
+++ src/main/java/com/sgg/suivisprod/controller/NewTaskController.java
@@ -1,12 +1,12 @@
 package com.sgg.suivisprod.controller;
 
+import static com.sgg.suivisprod.utils.AppCont.NEW_TASK_PATH;
+import static com.sgg.suivisprod.utils.AppCont.TASK_PATH;
+import static com.sgg.suivisprod.utils.AppCont.TASK_VIEW;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static com.sgg.suivisprod.utils.AppCont.TASK_VIEW;
-import static com.sgg.suivisprod.utils.AppCont.TASK_PATH;
-import static com.sgg.suivisprod.utils.AppCont.NEW_TASK_PATH;
 
 @Controller
 @RequestMapping(TASK_PATH)
