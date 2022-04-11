f39974e024404ae06a0c8ed9e089c9d0ef834449
root <root@debian>
2022-04-11 18:59:48 +0300

    extract pagination logic to an helper class

--- src/main/java/com/sgg/suivisprod/controller/HistoryController.java
+++ src/main/java/com/sgg/suivisprod/controller/HistoryController.java
@@ -2,6 +2,8 @@ package com.sgg.suivisprod.controller;
 
 import static com.sgg.suivisprod.utils.AppCont.HISTORY_PATH;
 import static com.sgg.suivisprod.utils.AppCont.HISTORY_VIEW;
+import static com.sgg.suivisprod.utils.PaginationHelper.ROW_LIMIT;
+import static com.sgg.suivisprod.utils.PaginationHelper.PAGINATION_LIMIT;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -19,6 +21,7 @@ import com.sgg.suivisprod.domain.Task;
 import com.sgg.suivisprod.domain.User;
 import com.sgg.suivisprod.repository.TaskRepository;
 import com.sgg.suivisprod.repository.UserRepository;
+import com.sgg.suivisprod.utils.PaginationHelper;
 
 @Controller
 public class HistoryController {
@@ -29,78 +32,24 @@ public class HistoryController {
 	@Autowired
 	UserRepository userRepository;
 
-	private static final int ROW_LIMIT        = 8;
-	private static final int PAGINATION_LIMIT = 5;
-
 	@GetMapping(HISTORY_PATH)
 	public String history(Model viewModel, Principal userPrinicipal, @RequestParam int currentPage) {
 		User user = userRepository.findByUsername(userPrinicipal.getName());
 
 		Pageable   page           = PageRequest.of(currentPage - 1, ROW_LIMIT);
 		List<Task> taskListToview = taskRepository.findByTaskType(user.getUserId(), "done", page);
+		int        totalTask      = taskRepository.countAllTask(user.getUserId(), "done");
 
-		int           totalPageNumber = getPageNumber(taskRepository.countAllTask(user.getUserId(), "done"));
-		List<Integer> paginationList  = buildPaginationList(currentPage, totalPageNumber);
+		PaginationHelper pagination = new PaginationHelper(totalTask, currentPage);
+		pagination.build();
+
+		int           totalPageNumber = pagination.getTotalPageNumber();
+		List<Integer> paginationList  = pagination.getPagination();
 
 		viewModel.addAttribute("tasks", taskListToview);
 		viewModel.addAttribute("totalPageNumber", totalPageNumber);
 		viewModel.addAttribute("paginationList", paginationList);
 		return HISTORY_VIEW;
-	}
-
-	/**
-	 * 
-	 * @param totalTaskCount
-	 * @return int of total page number
-	 */
-	private int getPageNumber(int totalTaskCount) {
-		int totalPageNumber = totalTaskCount / ROW_LIMIT;
-		return (totalPageNumber) == 0 ? 1 : totalPageNumber;
-	}
-
-	/**
-	 * Build 1-based pagination list for history view pagination
-	 * 
-	 * @param currentPage
-	 * @param totalPageNumber
-	 * @return list of Integer
-	 */
-	private List<Integer> buildPaginationList(int currentPage, int totalPageNumber) {
-		List<Integer> pageList       = new ArrayList<>();
-		int           pageRangeStartIndex = getPageRangeStartIndex(currentPage);
-
-		if (totalPageNumber > PAGINATION_LIMIT && pageRangeStartIndex > totalPageNumber - PAGINATION_LIMIT) {
-			pageRangeStartIndex = totalPageNumber - PAGINATION_LIMIT + 1;
-		}
-
-		for (int i = 1; i <= PAGINATION_LIMIT; i++) {
-			pageList.add(pageRangeStartIndex);
-			if (pageRangeStartIndex == totalPageNumber) {
-				break;
-			}
-			pageRangeStartIndex++;
-		}
-		return pageList;
-	}
-
-	/**
-	 * 
-	 * @param currentPage
-	 * @return
-	 */
-	private int getPageRangeStartIndex(int currentPage) {
-		String stringTotalPageNumber = Integer.toString(currentPage);
-		int    value             = Integer.parseInt(stringTotalPageNumber.substring(stringTotalPageNumber.length() - 1));
-		int    startRangeIndex        = 0;
-
-		if (value <= 5) {
-			startRangeIndex = (stringTotalPageNumber.length() < 2) ? currentPage - value + 1 : currentPage - 4;
-		}
-		else if (value > 5) {
-			startRangeIndex = currentPage - value + 6;
-		}
-
-		return startRangeIndex;
 	}
 
 }
---
+++ src/main/java/com/sgg/suivisprod/utils/PaginationHelper.java
@@ -0,0 +1,105 @@
+package com.sgg.suivisprod.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaginationHelper {
+
+	public static final int ROW_LIMIT        = 8;
+	public static final int PAGINATION_LIMIT = 5;
+
+	private List<Integer> pagination;
+	private int           totalPageNumber;
+	private int           currentPage;
+	private int           totalTaskCount;
+
+	/**
+	 * @return the totalTaskCount
+	 */
+	public int getTotalTaskCount() {
+		return totalTaskCount;
+	}
+
+	/**
+	 * @param totalTaskCount the totalTaskCount to set
+	 */
+	public void setTotalTaskCount(int totalTaskCount) {
+		this.totalTaskCount = totalTaskCount;
+	}
+
+	/**
+	 * @return the totalPageNumber
+	 */
+	public int getTotalPageNumber() {
+		return totalPageNumber;
+	}
+
+	/**
+	 * @return the pagination
+	 */
+	public List<Integer> getPagination() {
+		return pagination;
+	}
+
+	public PaginationHelper(int totalTaskCount, int currentPage) {
+		this.totalTaskCount = totalTaskCount;
+		this.currentPage = currentPage;
+		this.pagination = new ArrayList<>();
+	}
+
+	public List<Integer> build() {
+		calculatePageNumber();
+		buildPaginationList();
+		return this.pagination;
+	}
+
+	private void calculatePageNumber() {
+		int pageNumber = this.totalTaskCount / ROW_LIMIT;
+		this.totalPageNumber = (pageNumber) == 0 ? 1 : pageNumber;
+	}
+
+	/**
+	 * Build 1-based pagination list for history view pagination
+	 * 
+	 * @param currentPage
+	 * @param totalPageNumber
+	 * @return list of Integer
+	 */
+	private void buildPaginationList() {
+		int pageRangeStartIndex = getPageRangeStartIndex();
+
+		if (totalPageNumber > PAGINATION_LIMIT && pageRangeStartIndex > totalPageNumber - PAGINATION_LIMIT) {
+			pageRangeStartIndex = totalPageNumber - PAGINATION_LIMIT + 1;
+		}
+
+		for (int i = 1; i <= PAGINATION_LIMIT; i++) {
+			this.pagination.add(pageRangeStartIndex);
+			if (pageRangeStartIndex == totalPageNumber) {
+				break;
+			}
+			pageRangeStartIndex++;
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param currentPage
+	 * @return
+	 */
+	private int getPageRangeStartIndex() {
+		String stringTotalPageNumber = Integer.toString(currentPage);
+		int    value                 = Integer
+				.parseInt(stringTotalPageNumber.substring(stringTotalPageNumber.length() - 1));
+		int    startRangeIndex       = 0;
+
+		if (value <= 5) {
+			startRangeIndex = (stringTotalPageNumber.length() < 2) ? 1 : currentPage - 4 - value;
+		}
+		else if (value > 5) {
+			startRangeIndex = currentPage - value + 6;
+		}
+
+		return startRangeIndex;
+	}
+}
