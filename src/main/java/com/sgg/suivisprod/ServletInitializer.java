4acf9dd3add17120f178bed01bd2acdc222c0d78
Eric RANDRIANARISOA <raneric@gmail.com>
2022-05-01 20:35:43 +0300

    add ServletInitializer for tomcat server

---
+++ src/main/java/com/sgg/suivisprod/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sgg.suivisprod;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SuivisprodApplication.class);
+	}	
+}
