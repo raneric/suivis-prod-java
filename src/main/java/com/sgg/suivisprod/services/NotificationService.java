9eff6eaa08c2c8d950e5322fda9f6bd44398536f
Eric RANDRIANARISOA <raneric@gmail.com>
2022-05-26 17:24:22 +0300

    fix import

--- src/main/java/com/sgg/suivisprod/services/NotificationService.java
+++ src/main/java/com/sgg/suivisprod/services/NotificationService.java
@@ -1,5 +1,9 @@
 package com.sgg.suivisprod.services;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
 import com.sgg.suivisprod.domain.Notification;
 import com.sgg.suivisprod.utils.NotificationType;
 
